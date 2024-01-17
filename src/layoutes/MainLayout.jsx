2a97685a9bb2e717b8cb5d46a77693d3728bcd33
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-01-17 18:23:09 +0600

    add footer section

--- src/layoutes/MainLayout.jsx
+++ src/layoutes/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../pages/Shared/Footer/Footer';
 
 const MainLayout = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
