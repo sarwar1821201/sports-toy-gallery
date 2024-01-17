7c02a76291f72a040d3be821a4cb9374f21dc635
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-01-18 01:49:24 +0600

    create authprovider for authentication

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from '../Firebase/firebase.config';
 
-const AuthProvider = () => {
+
+const auth=getAuth(app)
+
+export const AuthContext= createContext(null)
+
+const AuthProvider = ({children}) => {
+  
+    const [user, setUser] =useState(null);
+    const [loading, setLoading]= useState(true)
+
+    const createUser= (email,password) =>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+
+     const authInfo={
+        user, loading,createUser
+     }
+  
+
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo} >
+            {children}
+        </AuthContext.Provider>
     );
 };
 
