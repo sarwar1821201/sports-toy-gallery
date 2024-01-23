c65f50dc6d124035fec8c788416b40e078b6fa1a
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-01-24 03:54:29 +0600

    set booking data dynamically

--- src/pages/PrivatePage/MyToy/MyToy.jsx
+++ src/pages/PrivatePage/MyToy/MyToy.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { AuthContext } from '../../../provider/AuthProvider';
+import MyToyRow from './MyToyRow';
 
 const MyToy = () => {
    const {user} = useContext(AuthContext);
@@ -15,11 +16,48 @@ const MyToy = () => {
           setBookings(data)
      } )
 
-   } ,[] )
+   } ,[url] )
 
     return (
         <div>
             <h2 className='mt-3 mb-4 text-center text-3xl' >Here Are Yours Toys Collections : {bookings.length} </h2>
+
+            <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox" />
+          </label>
+        </th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      
+      {
+         bookings.map ( (booking ) => <MyToyRow  
+           key={booking._id}
+           booking={booking}
+         > 
+
+
+         </MyToyRow>   )
+      }
+     
+    </tbody>
+    
+   
+    
+  </table>
+</div>
+
+
         </div>
     );
 };
